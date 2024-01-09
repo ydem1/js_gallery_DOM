58dd6dccfd9e4503d7d5cc233e881e4c9c71b4a1
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-09 11:33:42 +0200

    make build

---
+++ index.4f0975d9.js
@@ -0,0 +1,4 @@
+var e=document.querySelector("#largeImg");// eslint-disable-next-line no-shadow
+document.querySelector("#thumbs").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");r&&(t.preventDefault(),e.setAttribute("src",r.getAttribute("href")))});//# sourceMappingURL=index.4f0975d9.js.map
+
+//# sourceMappingURL=index.4f0975d9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const largeImgElem = document.querySelector("#largeImg");
+const galleryListElem = document.querySelector("#thumbs");
+// eslint-disable-next-line no-shadow
+galleryListElem.addEventListener("click", (event)=>{
+    const item = event.target.closest(".list-item__link");
+    if (item) {
+        event.preventDefault();
+        largeImgElem.setAttribute("src", item.getAttribute("href"));
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
